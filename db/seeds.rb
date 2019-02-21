1507ee467baedecd332994ccf40bce72a1956c62
Jessica Anderson <jessie.leigh30@gmail.com>
2019-02-20 17:04:09 -0700

    add items

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+5.times do 
+  d = Department.create(
+    name: Faker::Company.industry
+  )
+
+5.times do
+  Item.create(
+    department_id: d.id,
+    name: Faker::TvShows::NewGirl.character,
+    description: Faker::TvShows::MichaelScott.quote,
+    price: Faker::Number.decimal(2)
+  )
+  end
+end
+
+puts 'seeded'
